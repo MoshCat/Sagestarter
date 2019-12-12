e45ed5e3da0bc70f37e809c24dc9277d5226ce8b
Bas Middelham <bas@middelham.nl>
2019-12-12 11:08:38 +0100

    Add webfontloader

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,12 @@
+var WebFont = require('webfontloader');
+
 export default {
   init() {
-    // JavaScript to be fired on all pages
+    WebFont.load({
+      google: {
+        families: ['Roboto:300,400,500,700&display=swap'],
+      },
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
